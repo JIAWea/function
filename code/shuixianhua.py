7b04d59bbc0c75dc066e4097f0cc465e45f82308
100-chicken <37564116+JIAWea@users.noreply.github.com>
2018-08-31 11:39:56 +0800

    Create shuixianhua.py

---
+++ code/shuixianhua.py
@@ -0,0 +1,12 @@
+"""
+找出100~999之间的所有水仙花数
+水仙花数是各位立方和等于这个数本身的数
+如: 153 = 1**3 + 5**3 + 3**3
+"""
+
+for number in range(100,1000):
+  ge = number % 10
+  shi = number // 10 % 10
+  bai = number // 100
+  if ge ** 3 + shi ** 3 + bai ** 3 == 100:
+    print(number)
